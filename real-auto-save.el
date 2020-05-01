d9bc8003e99c1b91a397c765438ba4148bf73aad
conao3 <conao3@gmail.com>
2020-05-02 03:48:35 +0900

    remove double newline and only semicolon lines

--- real-auto-save.el
+++ real-auto-save.el
@@ -1,6 +1,5 @@
 ;;; real-auto-save.el --- Automatically save your all your buffers/files at regular intervals.
 
-
 ;; Copyright (C) 2008-2015, Chaoji Li, Anand Reddy Pandikunta
 
 ;; Author: Chaoji Li <lichaoji AT gmail DOT com>
@@ -26,33 +25,19 @@
 ;;; Commentary:
 
 ;; Put this file in a folder where Emacs can find it.
-;;
+
 ;; Add following lines to your .emacs initialization file
 ;; to enable auto save in all programming modes.
-;;
+
 ;;     (require 'real-auto-save)
 ;;     (add-hook 'prog-mode-hook 'real-auto-save-mode)
-;;
-;;
+
 ;; Auto save interval is 10 seconds by default.
 ;; You can change it to whatever value you want at any point.
-;;
+
 ;;     (setq real-auto-save-interval 5) ;; in seconds
-;;
-;;
-;; On Makefile, Emacs confirming really save every and every insert
-;; <tab> to edit it.
-;; Suppress this confirming, call `real-auto-save-activate-advice'
-;; on your init.el
-;;
-;;     (real-auto-save-activate-advice)
-;;
-;; If disable this advice, call `real-auto-save-remove-advice'.
-;;
-;;
 
 ;;; Code:
-
 
 (defgroup real-auto-save nil
   "Save buffers automatically."
@@ -107,7 +92,6 @@
   (if real-auto-save-mode
       (real-auto-save--setup)
     (real-auto-save--teardown)))
-
 
 (provide 'real-auto-save)
 ;;; real-auto-save.el ends here
